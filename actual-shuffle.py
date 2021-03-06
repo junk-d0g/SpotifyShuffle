0d1f09d0a1976184e69acc984837b820fe189b61
Nick B <35877456+ambeloe@users.noreply.github.com>
2021-03-06 17:00:23 -0500

    chugnus song getting chunk
    much faster as it gets songs in chunks of 50 instead of 1

--- actual-shuffle.py
+++ actual-shuffle.py
@@ -6,9 +6,14 @@ import sys
 from spotipy.oauth2 import SpotifyOAuth
 import spotipy.util as util
 from json.decoder import JSONDecodeError
+import json
 
 username = sys.argv[1]
 scope = 'user-read-private user-read-playback-state user-modify-playback-state user-library-read'
+
+os.environ["SPOTIPY_CLIENT_SECRET"] = "bc1a1e0ca87d45d0b8cd0b4faca09217"
+os.environ["SPOTIPY_CLIENT_ID"] = "cfc38a177d8c44d6ab871b93b0f4f072"
+os.environ["SPOTIPY_REDIRECT_URI"] = "https://google.us/"
 
 try:
     token = util.prompt_for_user_token(username, scope)
@@ -21,19 +26,26 @@ spotifyObject = spotipy.Spotify(auth=token)
 tracks = spotifyObject.current_user_saved_tracks()['items']
 songs = []
 
-run = True
+amount = 50
+
 off = 0
 print("starting indexing")
-while run:
+while True:
     print("adding index ",end='')
     print(off)
     try:
-        spotifyObject.current_user_saved_tracks(limit=1,offset=off)
-        uri = spotifyObject.current_user_saved_tracks(limit=1, offset=off)['items'][0]['track']['uri']
-        songs.append(uri)
-        off+=1
+        hog = spotifyObject.current_user_saved_tracks(limit=amount, offset=off)['items']
+        if len(hog) == 0:
+            break
+        for track in hog:
+            #print(json.dumps(track, indent=4))
+            #print("#############################################################")
+            uri = track['track']['uri']
+            #print(uri)
+            #print("%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%")
+            songs.append(uri)
+        off+=amount
     except Exception:
-        run = False
         break
 
 print("finished indexing")
@@ -41,6 +53,6 @@ print("adding random indexes to queue")
 for i in range(len(songs)):
     rand = random.randint(0,len(songs)-1)
     spotifyObject.add_to_queue(songs[rand])
-    songs.remove(songs[rand])
+    del songs[rand]
 
 #['items'][0]['track']['track_number']
